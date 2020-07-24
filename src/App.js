97113e2470e44c19714c07b4235cce7c9fcac447
skyraider <akashyadav256526@gmail.com>
2020-07-24 22:18:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+/// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/" component={login} />
+          <Route exact path="/" component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/signup.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class signup extends Component {
+  render() {
+    return <div>SignUp</div>;
+  }
+}
